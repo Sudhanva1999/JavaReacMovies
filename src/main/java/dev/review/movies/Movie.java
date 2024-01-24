de00ddd33f7640b6828a20787e2a4d21f89ba7f1
Sudhanva1999 <49947142+Sudhanva1999@users.noreply.github.com>
2024-01-24 15:09:05 -0500

    Assing rest of the code

--- src/main/java/dev/review/movies/Movie.java
+++ src/main/java/dev/review/movies/Movie.java
@@ -1,4 +1,44 @@
 package dev.review.movies;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collection = "movies")
+@Data // Lombox tag that takes care of all getters and setters.
+@AllArgsConstructor // Creates a constructor with all arguments.
+@NoArgsConstructor // Creates a constructor with no arguments.
 public class Movie {
+  @Id
+  private ObjectId id;
+  private String imdbId;
+  private String title;
+  private String releaseDate;
+  private String trailerLink;
+  private String poster;
+  private List<String> backdrops;
+  private List<String> genres;
+
+  // This will cause mongo to store only review ids and actual reviews will be stored as
+  // a separate collection.
+  @DocumentReference // Manual reference relationship.
+  private List<Review> reviews;
+
+  public Movie(String imdbId, String title, String releaseDate, String trailerLink,
+               String poster, List<String> backdrops, List<String> genres) {
+    this.imdbId = imdbId;
+    this.title = title;
+    this.releaseDate = releaseDate;
+    this.trailerLink = trailerLink;
+    this.poster = poster;
+    this.backdrops = backdrops;
+    this.genres = genres;
+  }
 }
---
+++ src/main/java/dev/review/movies/MovieService.java
@@ -0,0 +1,22 @@
+package dev.review.movies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+
+  @Autowired // Framework auto initializes the class.
+  private MovieRepository repository;
+
+  public List<Movie> findAllMovies() {
+    return repository.findAll();
+  }
+
+  // Optional lets java know that the method might return null.
+  public Optional<Movie> findMovieByImdbId(String imdbId) {
+    return repository.findMovieByImdbId(imdbId);
+  }
+}
